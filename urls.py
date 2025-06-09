56fb2f4f98f3760deeb6678c5dfeeab312497970
Tikhonuk <tihonuk2006@gmail.com>
2025-06-09 13:56:24 +0300

    Исправление работоспособности локальной страницы

--- urls.py
+++ urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 
-app_name = 'catalog'  # чтобы ссылки работали с namespace
+app_name = 'catalog'  # добавляем app_name!
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('composition/<int:pk>/', views.composition_detail, name='composition_detail'),
-    path('composition/add/', views.composition_add, name='composition_add'),
-    path('composition/<int:pk>/edit/', views.composition_edit, name='composition_edit'),
-    path('composition/<int:pk>/delete/', views.composition_delete, name='composition_delete'),
+
+    # Пока комментируем — чтобы не было ошибки
+    # path('composition/add/', views.add_composition, name='add_composition'),
+    # path('composition/<int:pk>/edit/', views.edit_composition, name='edit_composition'),
+    # path('composition/<int:pk>/delete/', views.delete_composition, name='delete_composition'),
 ]
