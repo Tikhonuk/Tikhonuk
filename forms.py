856118215dde313af75c16382917ce908f7d8f29
Tikhonuk <tihonuk2006@gmail.com>
2025-06-09 15:38:23 +0300

    Дизайн

--- forms.py
+++ forms.py
@@ -4,4 +4,11 @@ from .models import Composition
 class CompositionForm(forms.ModelForm):
     class Meta:
         model = Composition
-        fields = ['title', 'album', 'genre', 'duration', 'audio_file']  # не обязательно все, можно меньше
+        fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'album': forms.Select(attrs={'class': 'form-select'}),
+            'genre': forms.Select(attrs={'class': 'form-select'}),
+            'duration': forms.TimeInput(format='%H:%M:%S', attrs={'class': 'form-control', 'type': 'time'}),
+            'audio_file': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
