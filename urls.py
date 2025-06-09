5ac45e0303ae63820fdef3ee23d7b49829ca19d0
Tikhonuk <tihonuk2006@gmail.com>
2025-06-09 14:21:36 +0300

    Работающий функционал на детальной и других страницах

--- urls.py
+++ urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'catalog'  # добавляем app_name!
+app_name = 'catalog'  # важная строка!
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('composition/<int:pk>/', views.composition_detail, name='composition_detail'),
-
-    # Пока комментируем — чтобы не было ошибки
-    # path('composition/add/', views.add_composition, name='add_composition'),
-    # path('composition/<int:pk>/edit/', views.edit_composition, name='edit_composition'),
-    # path('composition/<int:pk>/delete/', views.delete_composition, name='delete_composition'),
+    path('composition/add/', views.composition_add, name='composition_add'),
+    path('composition/<int:pk>/edit/', views.composition_edit, name='composition_edit'),
+    path('composition/<int:pk>/delete/', views.composition_delete, name='composition_delete'),
 ]
